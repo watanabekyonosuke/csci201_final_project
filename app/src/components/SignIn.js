9ef05a7ba0e5117628edf7dae8ed5ea32b18b310
Miles Kirkbride <mkirkbri@usc.edu>
2024-04-14 22:08:03 -0700

    linked

---
+++ app/src/components/SignIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SiteHeader.css';
+
+const SignIn = () => {
+
+    return (
+        <div className="SignInContainer">
+            
+        </div>
+
+    );
+};
+
+export default SignIn;
