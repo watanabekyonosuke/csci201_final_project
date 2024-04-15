318d1d0974960d5375a18efd08430321b16d572e
Miles Kirkbride <mkirkbri@usc.edu>
2024-04-14 21:56:30 -0700

    multipage routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { ReactDOM } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import SiteHeader from './components/SiteHeader';
 import Landing from './components/Landing';
 import Forum from './components/Forum';
@@ -13,9 +13,11 @@ function App() {
         <header className="App-header">
           <SiteHeader />
         </header>
-        <div className="Content">
-          <Landing />
-        </div>
+        <Routes>
+          <Route path="/" element={<SiteHeader />} />
+          <Route path="Forum" element={<ForumHeader />}/>
+          <Route path="Landing" element={<Landing />}/>
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- app/src/components/SiteHeader.js
+++ app/src/components/SiteHeader.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 import './SiteHeader.css';
 
 const SiteHeader = () => {
 
     return (
+        <>
         <div className="nav-container">
             <header className="site-header">
                 <div className="site-header__left">
                     <h1 className="site-header__title">CSOasis</h1>
                 </div>
                 <div className="site-header__right">
-                    <a href="SignIn.html" className="site-header__button">SIGN IN</a>
-                    <button className="site-header__button">JOIN</button>
+                    <li className="site-header__button"><Link to="/Forum">Forum</Link></li>
+                    <li className="site-header__button"><Link to="/Landing">Landing</Link></li>
                 </div>
             </header>
         </div>
+
+        <Outlet />
+        </>
     );
 };
 
