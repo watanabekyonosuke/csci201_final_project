a6ef004a2f9fffffc7d24b4e078ec750590ab6e1
Miles Kirkbride <mkirkbri@usc.edu>
2024-04-20 18:37:09 -0700

    separate backend and frontend code - 'app' has react stuff

---
+++ SQL scripts/ForumDatabase.sql
@@ -0,0 +1,35 @@
+DROP DATABASE IF EXISTS ForumData;
+CREATE DATABASE ForumData;
+USE ForumData;
+CREATE TABLE ForumGroups (
+    fgid INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE User (
+    userid INT PRIMARY KEY,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+
+CREATE TABLE ForumDiscussions (
+    titleid INT PRIMARY KEY,
+    title VARCHAR(100),
+    fgid INT,
+    userid INT,
+    post VARCHAR(2000),
+    creationtime DATETIME,
+    FOREIGN KEY (fgid) REFERENCES ForumGroups(fgid),
+    FOREIGN KEY (userid) REFERENCES User(userid)
+);
+
+CREATE TABLE Comments (
+    commentid INT PRIMARY KEY,
+    userid INT,
+    comment VARCHAR(2000),
+    titleid INT,
+    creationtime DATETIME,
+    FOREIGN KEY (userid) REFERENCES User(userid),
+    FOREIGN KEY (titleid) REFERENCES ForumDiscussions(titleid)
+);
