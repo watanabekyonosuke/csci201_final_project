fe66b4e9340c8620549c3514be2ceeb487ef87ce
Miles Kirkbride <mkirkbri@usc.edu>
2024-04-26 21:56:59 -0700

    button + createcomment

---
+++ app/src/components/CreateComment.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import './CreateComment.css';
+
+const CreateComment = () => {
+    // use the states of the currently filled boxes
+    const navigate = useNavigate();
+    const [content, setContent] = useState('');
+
+
+    const handleCommentDiscussion = (event) => {
+        event.preventDefault();
+
+        // set up data to be sent
+        const data = {
+            content: content,
+            hours: new Date().getHours(), // Assuming you want to set the current hour
+            tid: parseInt(localStorage.getItem("tid")) // Assuming tid is stored in localStorage
+        };
+
+        // set up request to post discussion through CreateForumPostServlet
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3001/ProjectBackend/CreateCommentServlet');
+        xhr.setRequestHeader('Content-Type','application/json');
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                alert("Comment Posted!");
+                navigate("/Dicussion");
+            } else if (xhr.status === 400) {
+                alert("Please fill in all fields");
+                navigate("/CreateComment");
+            } else if (xhr.status === 401) {
+                alert("Something went wrong. Please try again later");
+                navigate("/Dicussion");
+            }
+        }
+        xhr.onerror = () => {
+            console.error('Error posting comment:', xhr.statusText);
+        };
+        xhr.send(JSON.stringify(data));
+    };
+    
+    return (
+        <div className='create-comment'>
+
+            BALSLS
+
+            <form className='comment-form' onSubmit={handleCommentDiscussion}>
+            
+                <div>
+                <label>
+                    <input
+                        type="text"
+                        id="body-input"
+                        placeholder="Comment..."
+                        className="body-input"
+                        value = {content}
+                        onChange={(e)=>setContent(e.target.value)}
+                    />
+                </label>
+                </div>
+                
+                <div id='submission'>
+                    <button>
+                        Submit
+                    </button>
+                </div>
+            </form>
+
+            <div className="discussion-container">
+                <Link to="/Discussion" className="back-button">&#8592;</Link>
+            </div>
+
+        </div>
+    );
+}
+
+export default CreateComment;
--- app/src/components/Discussion.js
+++ app/src/components/Discussion.js
@@ -1,89 +1,104 @@
 import React, { useState, useEffect } from 'react';
 import { FaRegThumbsUp } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
 import {Link} from "react-router-dom";
 import './Discussion.css';
 
 const Discussion = () => {
+    var loggedIn = localStorage.getItem("uid");
+    const navigate = useNavigate();
+    const [comments, setComments] = useState([]);
 
-    const [comments, setComments] = useState([]);
+    useEffect(() => {
+        fetchComments();
+    }, []);
 
-    useEffect(() => {
-        fetchComments();
-    }, []);
+    // A handler that sends a like/unlike action to the server
+    const handleLikeClick = (commentId, isLiked) => {
+        // Optimistic UI update
+        setComments(commentDiscussions =>
+        commentDiscussions.map(c =>
+            c.titleid === commentId ? { ...c, likes: c.likes + (isLiked ? -1 : 1), liked: !isLiked } : c
+        )
+        );
 
-    // A handler that sends a like/unlike action to the server
-    const handleLikeClick = (commentId, isLiked) => {
-        // Optimistic UI update
-        setComments(commentDiscussions =>
-        commentDiscussions.map(c =>
-            c.titleid === commentId ? { ...c, likes: c.likes + (isLiked ? -1 : 1), liked: !isLiked } : c
-        )
-        );
+        // Here you would make an API call to the backend to update the likes
+        // For now, we'll just log to the console
+        console.log(`Comment ${commentId} liked status: ${!isLiked}`);
 
-        // Here you would make an API call to the backend to update the likes
-        // For now, we'll just log to the console
-        console.log(`Comment ${commentId} liked status: ${!isLiked}`);
+        // If the API call fails, you'll need to revert the UI changes
+        // This is where you would catch an error and revert if necessary
+    };
 
-        // If the API call fails, you'll need to revert the UI changes
-        // This is where you would catch an error and revert if necessary
-    };
+    const handleCreateCommentClick = () => {
+        if (!loggedIn) {
+            alert("Please Sign In or Register to Make a Post!");
+        } else {
+            navigate("/CreateComment");
+        }
+    }
 
 
-    const fetchComments = () => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://localhost:3001/ProjectBackend/CommentServlet');
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = () => {
-            if (xhr.status === 200) {
-                const data = JSON.parse(xhr.responseText);
-                console.log(data);
-                setComments(data);
-            } else {
-                console.error('Error fetching comments:', xhr.statusText);
-            }
-        };
-        xhr.onerror = () => {
-            console.error('Error fetching comments:', xhr.statusText);
-        };
-        const disc = parseInt(localStorage.getItem("tid"));
-        xhr.send(JSON.stringify({ titleid: disc}));
-    };
+    const fetchComments = () => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://localhost:3001/ProjectBackend/CommentServlet');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText);
+                console.log(data);
+                setComments(data);
+            } else {
+                console.error('Error fetching comments:', xhr.statusText);
+            }
+        };
+        xhr.onerror = () => {
+            console.error('Error fetching comments:', xhr.statusText);
+        };
+        const disc = parseInt(localStorage.getItem("tid"));
+        xhr.send(JSON.stringify({ titleid: disc}));
+    };
  
-    const refreshCommentList = () => {
-     // Call fetchDiscussions to refresh the forum list
-         fetchComments();
-     };
+    const refreshCommentList = () => {
+     // Call fetchDiscussions to refresh the forum list
+         fetchComments();
+     };
 
-    return (
-        <div className="comment-container">
-            <Link to="/ForumList" className="back-button">&#8592;</Link>
-            <div className="commentList">
-               {comments.map((comment, index) => (
-                    <div key={index} className="commentItem">
-                    <div className="commentAttributes">
-                        <div className="commentLikes">
-                            <button
-                                onClick={() => handleLikeClick(comment.titleid, comment.liked)}
-                                className={`like-button ${comment.liked ? 'liked' : ''}`}>
-                                <FaRegThumbsUp /> {comment.likes}
-                            </button>
-                        </div>
-                        <div className="commentAge">
-                            {comment.hours} hours ago
-                        </div>
-                        <div className="commentContent">
-                            {comment.content}
-                        </div>
-                        <div className="commentUser">
-                            Posted by: {comment.username}
-                        </div>
-                    </div>
-                </div>
-               ))}
-           </div>
-        </div>
-    );
+    return (
+        <div className="comment-container">
+            <Link to="/ForumList" className="back-button">&#8592;</Link>
+            <div className="commentList">
+            
+               {comments.map((comment, index) => (
+                    <div key={index} className="commentItem">
+                    <div className="commentTitle">{comment.title}</div>
+                    <div className="commentAttributes">
+                        <div className="commentLikes">
+                            <button
+                                onClick={() => handleLikeClick(comment.titleid, comment.liked)}
+                                className={`like-button ${comment.liked ? 'liked' : ''}`}>
+                                <FaRegThumbsUp /> {comment.likes}
+                            </button>
+                        </div>
+                        <div className="commentAge">
+                            {comment.hours} hours ago
+                        </div>
+                        <div className="commentContent">
+                            {comment.content}
+                        </div>
+                        <div className="commentUser">
+                            Posted by: {comment.username}
+                        </div>
+                    </div>
+                </div>
+                
+               ))}
+               <button id='create-comment' onClick={handleCreateCommentClick}>Add Comment</button> 
+           </div>
+        </div>
+    );
 }
 
 
 export default Discussion;
+
